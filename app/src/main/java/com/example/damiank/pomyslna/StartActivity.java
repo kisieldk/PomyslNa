97f403eceaf0fa7f5db47989e4b756a70952fa58
kisieldk <dkisielewski@gmail.com>
2015-01-18 14:19:27 +0100

    wyswietlanie przepisow

--- app/src/main/java/com/example/damiank/pomyslna/StartActivity.java
+++ app/src/main/java/com/example/damiank/pomyslna/StartActivity.java
@@ -1,34 +1,47 @@
 package com.example.damiank.pomyslna;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity (R.layout.activity_start)
 @OptionsMenu(R.menu.menu_start)
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends Activity {
 
-    @ViewById
-    public EditText login;
-    @ViewById
-    public EditText haslo;
+
 
     @Click
-    void btnLogujClicked(){
-        if (login.getText().length() <  3 || haslo.getText().length() < 3) {
-            Toast.makeText(this,getString(R.string.logerror),Toast.LENGTH_LONG ).show();
+    void btnDodajClicked(){
 
-        }
-        goToMenuActivity();
+        LoginActivity_.intent(this).start();
 
     }
-    public void goToMenuActivity(){
+    @Click
+    void btnUlubioneClicked(){
+
+        MyRecActivity_.intent(this).start();
+
+    }
+    @Click
+    void btnPrzepisyClicked(){
+
+        OtherRecActivity_.intent(this).start();
+
+    }
+    @Click
+    void btnUsunClicked(){
 
         MenuActivity_.intent(this).start();
+
     }
+
+
 }
