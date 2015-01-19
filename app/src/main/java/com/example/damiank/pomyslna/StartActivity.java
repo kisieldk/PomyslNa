9e9f7f46234627eefd26b6fe47bbddd6aa73d38b
kisieldk <dkisielewski@gmail.com>
2015-01-19 20:50:17 +0100

    usuwanie w pelni

--- app/src/main/java/com/example/damiank/pomyslna/StartActivity.java
+++ app/src/main/java/com/example/damiank/pomyslna/StartActivity.java
@@ -1,13 +1,17 @@
 package com.example.damiank.pomyslna;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.damiank.pomyslna.data.User;
+
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
@@ -15,19 +19,28 @@ import org.androidannotations.annotations.ViewById;
 @EActivity (R.layout.activity_start)
 @OptionsMenu(R.menu.menu_start)
 public class StartActivity extends Activity {
-
+@Extra
+User user;
 
 
     @Click
     void btnDodajClicked(){
-
-        LoginActivity_.intent(this).start();
-
+try {
+    if (user.sessionId != null) {
+        AddActivity_.intent(this).user(user).start();
+    }
+} catch (NullPointerException e){
+    LoginActivity_.intent(this).start();}
     }
     @Click
     void btnUlubioneClicked(){
 
-        MyRecActivity_.intent(this).start();
+        try {
+            if (user.sessionId != null) {
+                UlubioneActivity_.intent(this).user(user).start();
+            }
+        } catch (NullPointerException e){
+            LoginActivity_.intent(this).start();}
 
     }
     @Click
@@ -39,7 +52,12 @@ public class StartActivity extends Activity {
     @Click
     void btnUsunClicked(){
 
-        MenuActivity_.intent(this).start();
+        try {
+            if (user.sessionId != null) {
+                MyRecActivity_.intent(this).user(user).start();
+            }
+        } catch (NullPointerException e){
+            LoginActivity_.intent(this).start();}
 
     }
 
